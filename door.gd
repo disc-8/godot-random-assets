f118594adca0aaff4e4eacf046a9c7df07e2b768
snuskgubbe <130178512+snuskgubbe@users.noreply.github.com>
2023-04-15 20:28:14 +0200

    Create door.gd

---
+++ door.gd
@@ -0,0 +1,32 @@
+#this needs pretty spesiffix nodes: rotation pivot,interaction area,n da model itself
+extends Spatial
+
+onready var rotpivot=$rotpivot
+var STATE=0 # 1 - open; 0 - closed
+var canopen = 0
+var targetang=0.0
+export var openang=70.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	rotpivot.rotation.y = lerp_angle(rotpivot.rotation.y, targetang, 0.04)
+	if Input.is_action_just_pressed("interact") and canopen == 1:
+		match(STATE):
+			1:
+				targetang=deg2rad(0.0)
+				STATE=0
+			0:
+				targetang=deg2rad(openang)
+				STATE=1
+
+
+func _on_open_area_area_entered(area):
+	print("test")
+	canopen = 1
+
+func _on_open_area_area_exited(area):
+	canopen = 0
